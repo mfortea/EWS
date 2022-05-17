0b6b3cd39af1950336149c84ffd743e86f80c72e
mfortea <mforteadugo@gmail.com>
2022-05-17 13:20:27 +0200

    Cambios en el script

--- BASIC/BASIC-instalar.sh
+++ BASIC/BASIC-instalar.sh
@@ -19,6 +19,27 @@ function update-y-upgrade() {
   apt-get update -y &> $s_null 
 }
 
+function instalar-apache() {
+sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
+sudo apt-get install libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-idn php-pear php5-imagick php5-mcrypt php5-mysql php5-ps php5-pspell php5-recode php5-xsl -y
+sudo chown -R www-data:www-data /var/ww
+sudo a2enmod rewrite
+sudo php5enmod mcrypt
+sudo service apache2 restart
+}
+
+function instalar-mysql() {
+sudo apt-get install mysql-server mysql-client libmysqlclient15.dev -y
+}
+
+function instalar-phpmyadmin() {
+sudo apt-get install phpmyadmin -y
+sudo apt-get install apache2 libapache2-mod-php5 php5 mysql-server php-pear php5-mysql mysql-client mysql-server php5-mysql php5-gd -y
+
+}
+
+
+
 #### FUNCIÓN PRINCIPAL
 
 function instalar-basic(){
@@ -51,8 +72,6 @@ function instalar-basic(){
   echo
   update-y-upgrade
 
-
-
   # Tarea 2
   let "actual += 1"
   echo "-> Tarea: "$actual/$total
@@ -60,12 +79,35 @@ function instalar-basic(){
   echo
   instalar-dependencias
 
-  echo -e "$defecto"
+  # Tarea 3
+  let "actual += 1"
+  echo "-> Tarea: "$actual/$total
+  echo "   Instalando Apache..."
+  echo
+  instalar-apache
+
+  # Tarea 4
+  let "actual += 1"
+  echo "-> Tarea: "$actual/$total
+  echo "   Instalando mySQL..."
+  echo
+  instalar-mysql
+
+  # Tarea 5
+  let "actual += 1"
+  echo "-> Tarea: "$actual/$total
+  echo "   Instalando PHPMyAdmin..."
+  echo
+  instalar-phpmyadmin
+
 
   # Cuando la instalación termina
+  echo -e "$defecto"
   clear
+  echo -e "$cyan"
   figlet -c "EWS" 
   figlet -c "BASIC Pack"
+  echo -e "$defecto"
   echo -e "$verde"
   echo "           ***** LA INSTALACIÓN HA FINALIZADO CORRECTAMENTE *****              "
   echo -e "$defecto"
