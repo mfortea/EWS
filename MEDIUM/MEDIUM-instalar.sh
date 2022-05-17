8ce61fd7d8dd4bdd86e7877976f76b46a20ec018
mfortea <mforteadugo@gmail.com>
2022-05-17 19:20:58 +0200

    Corrección de ficheros

--- MEDIUM/MEDIUM-instalar.sh
+++ MEDIUM/MEDIUM-instalar.sh
@@ -60,10 +60,8 @@ function instalar-wordpress(){
   sudo systemctl restart apache2 &> $s_null 
 }
 
-function instalar-odoo(){
-sudo apt update
-sudo apt install git python3-pip build-essential wget python3-dev python3-venv python3-wheel libfreetype6-dev libxml2-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libjpeg-dev zlib1g-dev libpq-dev libxslt1-dev libldap2-dev libtiff5-dev libjpeg8-dev libopenjp2-7-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev
-sudo useradd -m -d /opt/odoo14 -U -r -s /bin/bash odoo14
+function instalar-odoo() {
+
 sudo apt install postgresql
 sudo su - postgres -c "createuser -s odoo14"
 sudo wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb
