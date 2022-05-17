98ba988f493591b120da734b9892f9cfbf1eb5ef
mfortea <mforteadugo@gmail.com>
2022-05-17 19:26:46 +0200

    Cambios

--- MEDIUM/MEDIUM-instalar.sh
+++ MEDIUM/MEDIUM-instalar.sh
@@ -61,11 +61,13 @@ function instalar-wordpress(){
 }
 
 function instalar-odoo() {
-
+sudo apt update
+sudo apt install -y git python3-pip build-essential wget python3-dev python3-venv python3-wheel libfreetype6-dev libxml2-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libjpeg-dev zlib1g-dev libpq-dev libxslt1-dev libldap2-dev libtiff5-dev libjpeg8-dev libopenjp2-7-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev
+sudo useradd -m -d /opt/odoo14 -U -r -s /bin/bash odoo14
 sudo apt install postgresql
 sudo su - postgres -c "createuser -s odoo14"
 sudo wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb
-sudo apt install ./wkhtmltox_0.12.5-1.bionic_amd64.deb
+sudo apt install -y./wkhtmltox_0.12.5-1.bionic_amd64.deb
 su - "odoo14" <<SHT
   git clone https://www.github.com/odoo/odoo --depth 1 --branch 14.0 /opt/odoo14/odoo
   cd /opt/odoo14
