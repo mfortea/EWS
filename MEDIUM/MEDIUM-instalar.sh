e3705df62825be9c59f765fe7d1e9e8e2a7cb762
mfortea <mforteadugo@gmail.com>
2022-05-17 19:33:50 +0200

    Tests

--- MEDIUM/MEDIUM-instalar.sh
+++ MEDIUM/MEDIUM-instalar.sh
@@ -62,7 +62,9 @@ function instalar-wordpress(){
 
 function instalar-odoo() {
 sudo apt update
-sudo apt install -y git python3-pip build-essential wget python3-dev python3-venv python3-wheel libfreetype6-dev libxml2-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libjpeg-dev zlib1g-dev libpq-dev libxslt1-dev libldap2-dev libtiff5-dev libjpeg8-dev libopenjp2-7-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev
+sudo apt install -y git python3-pip build-essential wget python3-dev python3-venv python3-wheel libfreetype6-dev 
+sudo apt install -y libxml2-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libjpeg-dev zlib1g-dev libpq-dev libxslt1-dev 
+sudo apt install -y libldap2-dev libtiff5-dev libjpeg8-dev libopenjp2-7-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev
 sudo useradd -m -d /opt/odoo14 -U -r -s /bin/bash odoo14
 sudo apt install postgresql
 sudo su - postgres -c "createuser -s odoo14"
