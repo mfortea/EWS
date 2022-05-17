2004041a4dca15308ca88994fb3d2ffdf44a1c08
mfortea <mforteadugo@gmail.com>
2022-05-17 13:22:22 +0200

    Cambios en el script

--- BASIC/BASIC-instalar.sh
+++ BASIC/BASIC-instalar.sh
@@ -20,12 +20,12 @@ function update-y-upgrade() {
 }
 
 function instalar-apache() {
-sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
-sudo apt-get install libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-idn php-pear php5-imagick php5-mcrypt php5-mysql php5-ps php5-pspell php5-recode php5-xsl -y
-sudo chown -R www-data:www-data /var/ww
-sudo a2enmod rewrite
-sudo php5enmod mcrypt
-sudo service apache2 restart
+sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y &> $s_null 
+sudo apt-get install libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-idn php-pear php5-imagick php5-mcrypt php5-mysql php5-ps php5-pspell php5-recode php5-xsl -y &> $s_null 
+sudo chown -R www-data:www-data /var/ww &> $s_null 
+sudo a2enmod rewrite &> $s_null 
+sudo php5enmod mcrypt &> $s_null 
+sudo service apache2 restart &> $s_null 
 }
 
 function instalar-mysql() {
